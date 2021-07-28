de3be928e638ba3603f041dc3b68d203cf43c67d
Justin Licari <justinlicari@Justins-MacBook-Air.local>
2021-07-27 23:03:45 -0400

    Image upload endpoint

--- src/server.js
+++ src/server.js
@@ -1,17 +1,33 @@
 import express from 'express'
 import { addJobToQueue } from './send'
 import { uploadToS3 } from './upload'
+import cors from 'cors'
+import multer from 'multer'
+import { upload } from './fileUtils'
 
 const app = express()
-const port = 3000
+const port = 8081
+
+app.use(cors())
 
 app.post('/upload', async (req, res) => {
-    const { user, fileName } = req.body
+    upload(req, res, function (err) {
+        if (err instanceof multer.MulterError) {
+            return res.status(500).json(err)
+        } else if (err) {
+            return res.status(500).json(err)
+        }
+   return res.status(200).send(req.file)
+
+ })
+    //console.log(req.file)
+    /*const { user, fileName } = req.body
     const file = files[fileName]
     const s3Response = await uploadToS3(user, file)
     const { bucket, key, location } = s3Response
 
-    addJobToQueue(bucket, key, location, res)
+    addJobToQueue(bucket, key, location, res)*/
+    //return res.status(200).send(req.file)
   })
  
 app.get('/', (req, res) => {
